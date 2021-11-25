580e8c2ed0296912bc544d0e3e6ab8c5b0515e90
Sergio Pedro <srosaped@outlook.com>
2021-11-25 17:37:34 +0000

    adds project structure and bootstrap styles

--- js/clientes.js
+++ js/clientes.js
@@ -57,18 +57,24 @@ function preencherTabela(){
         linha="<tr>"+
                   "<td>"+clientes[posicao].numerocliente+"</td>"+
                   "<td>"+clientes[posicao].nomecliente+"</td>"+
-                  "<td>"+
-                        "<button type='button' onclick='editar(this);'>Editar</button>&nbsp;"+
-                        "<button type='button' onclick='apagar(this);'>Apagar</button>"+
+                  "<td style='text-align:right'>"+
+                        "<button class='btn btn-success' type='button' onclick='editar(this);' style='width:40px;height:40px;'>"+
+                            "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-fill' viewBox='0 0 16 16'>"+
+                                "<path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z'/>"+
+                            "</svg>"+
+                        "</button>&nbsp;&nbsp;"+
+                        "<button class='btn btn-danger' type='button' onclick='apagar(this);' style='width:40px;height:40px;'>"+
+                            "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>"+
+                                "<path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>"+
+                            "</svg>"+
+                        "</button>"+
                   "</td>"+
               "</tr>";
 
         $(".table tbody").append(linha);
     }
-
-    $(".table tbody tr:even").css("background-color","rgb(37,37,38)");
-    $(".table tbody tr:odd").css("background-color","rgb(51,51,51)");
 }
+
 
 function editar(obj){
     //Mostro o formulário
--- js/testes.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    
-    for(var i=1; i<=10;i++) {
-        var num ="<td>"+i+"</td>"
-        $("#lista").append(num);
-    }
-
-});
-
